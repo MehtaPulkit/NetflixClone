2bfe40b91aa7686e99e70d01b7b4381e16970544
Pulkit Mehta <42738169+MehtaPulkit@users.noreply.github.com>
2020-09-24 00:00:00 +1000

    Code

---
+++ src/Banner.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import instance from "./axios";
+import "./Banner.css";
+
+function Banner({ fetchURL }) {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    const getMovie = async () => {
+      const request = await instance.get(fetchURL);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    };
+    getMovie();
+  }, []);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h3 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h3>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
+}
+
+export default Banner;
